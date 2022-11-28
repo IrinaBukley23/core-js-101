2c2ca82eda7bb1c2f8b5da512173577987aca2a1
Irina <irenserge@mail.ru>
2022-11-28 18:35:06 +0300

    feat: tasks functions & conditions done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -497,8 +497,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
@@ -549,7 +549,14 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
+function selectMany(/*  arr, childrenSelector */) {
+  // return arr.map((item) => {
+  //   if ((typeof item) === 'object') {
+  //     item.map(childrenSelector);
+  //   }
+  //   childrenSelector;
+  //   return;
+  // });
   throw new Error('Not implemented');
 }
 
